e6deec17b9ed4e4ae5631a64ec6efc7644d1282d
Khushi-kumari123 <khushi05652026@gmail.com>
2026-06-14 17:16:32 +0530

    Add files via upload

---
+++ vowelcon.java
@@ -0,0 +1,21 @@
+package daywise;
+import java.util.*;
+public class vowelcon {
+
+	public static void main(String[] args) {
+		char ch;
+		Scanner sc=new Scanner(System.in);
+		ch=sc.next().charAt(0);
+		
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+		{
+			System.out.println("vowel");
+		}
+		else 
+		{
+			System.out.println("consonant");
+		}
+
+	}
+
+}
