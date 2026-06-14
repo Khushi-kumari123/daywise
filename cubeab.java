1f5d6855b9d80ec7cb56519bc44a793be27a8d32
Khushi-kumari123 <khushi05652026@gmail.com>
2026-06-14 17:15:46 +0530

    Add files via upload

---
+++ cubeab.java
@@ -0,0 +1,14 @@
+package daywise;
+import java.util.*;
+public class cubeab {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=(a*a*a)*b;
+		System.out.println(c);
+
+	}
+
+}
